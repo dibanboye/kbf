cfd810a67b11a8f291e641ff1927b0265dafc3b9
zizhaozhang <mr.zizhaozhang@gmail.com>
2017-03-13 13:43:16 -0400

    randomNumber is done

--- hash/generatehash.h
+++ hash/generatehash.h
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <math.h>
 
+
+// Task 2: Implement the getPrime function.
 template <typename T>
 int isPrime(T n) // assuming n > 1
 {
@@ -32,7 +34,7 @@ int isPrime(T n) // assuming n > 1
 template <typename T>
 T getPrime(T n){
 
-    int c = 1;
+    T c = 1;
     while (1){
         if (isPrime(n+c)) { 
             return n+c;
@@ -40,4 +42,21 @@ T getPrime(T n){
         c += 1;
     }
 }
+
+// Task 3: Implement the randomNumber function.
+// Generate a random number between [start, end]. 
+// Note that start and end is included.
+template <typename T>
+T randomNumber(T start, T end){
+    double myRand = rand()/(1.0 + RAND_MAX); 
+    printf("my Rand %f", myRand);
+    printf("RAND_MAX %d", RAND_MAX);
+    T range = end - start + 1;
+    T random_num = (myRand * range) + start;
+    return random_num;
+}
+
+//Task4: rabinHash
+
+
 
--- hash/test.cpp
+++ hash/test.cpp
@@ -15,10 +15,16 @@ int main(){
 
     s = getPrime(n);
 
-    printf("the prime is %d", s);
+    printf("the prime is %d\n", s);
+
+    int k;
+    for (int i; i < 10; i++){
+        k = randomNumber(0,10);
+        printf("the random number is %d\n", k);
+    }
+
+
 
     return 1;
-
-
 }
 
